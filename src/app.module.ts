46f0fbf8dac41fff4a182a80cc621ac691486e86
Jorge Fernandez <fsjorgeluis@gmail.com>
2022-06-24 21:04:37 -0400

    Add configuration through mongodb connection string and its unitary test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NoSqlModule } from './configurations/database/no-sql/no-sql.module';
+import { NoSqlService } from './configurations/database/no-sql/no-sql.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [NoSqlModule],
+      useFactory: async (mongodb: NoSqlService) => ({
+        uri: mongodb.uri,
+      }),
+      inject: [NoSqlService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /*
+   * Logger can shown the above logs types:
+   * ['error', 'warn', 'debug', 'log', 'verbose' ]
+   */
+  const app = await NestFactory.create(AppModule, {
+    logger: ['warn', 'error', 'debug'],
+    cors: true,
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const logger = new Logger();
+
+  await app.listen(port || 3000);
+  logger.debug(`🚀 API launched on: ${await app.getUrl()}`);
 }
 bootstrap();
